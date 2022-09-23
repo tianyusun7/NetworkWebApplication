e44c3f1267ada388fbcd7fba04e6f1adbfac7645
tianyusun7 <92064938+tianyusun7@users.noreply.github.com>
2022-09-23 14:08:04 -0700

    Add files via upload
    frontend

---
+++ around/constants/constants.go
@@ -0,0 +1,10 @@
+package constants
+
+const (
+    USER_INDEX = "user"
+    POST_INDEX = "post"
+    ES_URL = "http://10.138.0.2:9200"
+    ES_USERNAME = "tianyusun"
+    ES_PASSWORD = "123456"
+    GCS_BUCKET = "tianyusun7-bucket"
+)
